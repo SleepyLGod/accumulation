10425cc2ee417dc00ca446452791a18083e8f7d4
odin <2441164168lhd@gmail.com>
2022-08-24 01:00:46 +0800

    202104-4 inited.

---
+++ algorithm/self_training_records/csp/202104-4.cpp
@@ -0,0 +1,57 @@
+/**
+ * @file 202104-4.cpp
+ * @author odng
+ * @brief 
+ * @version 0.1
+ * @date 2022-08-24
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <iostream>
+#include <bits/stdc++.h>
+#define forEachPlus(i, a, b) for(int i = (a); i <= (b); ++i)
+#define forEachMinus(i, a, b) for(int i = (a); i >= (b); --i)
+typedef long long ll;
+inline int readInt() {
+  int ans = 0;
+  int symbol = 1;
+  char ch = getchar();
+  while (ch < '0' || ch > '9') {
+    if (ch == '-') {
+        symbol = -1;
+    }
+    ch = getchar();
+  }
+  while(ch >= '0' && ch <= '9') {
+    ans = (ans << 3) + (ans << 1) + ch - '0';
+    ch = getchar();
+  }
+  return ans * symbol;
+} 
+inline ll readLong() {
+    ll ans = 0;
+    int symbol = 1;
+    char ch = getchar();
+    char las = ' ';
+    while(!isdigit(ch)) { // isdigit() 判断某个字符型是否为数字
+        las = ch;
+        ch = getchar();
+    }
+    while (isdigit(ch)) {
+        ans = (ans << 1) + (ans << 3) + ch - '0';
+        ch = getchar();
+    }
+    if (las == '-') {
+        symbol = -1;
+    }
+    return ans * symbol;
+}
+
+int main () {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+    
+    return 0;
+}
